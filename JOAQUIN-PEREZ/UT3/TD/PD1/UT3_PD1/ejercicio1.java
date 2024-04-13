e382527f12f0a8d77dbde8ecf929b3fad69a7f14
RowletGA <joaquin.perez@correo.ucu.edu.uy>
2024-04-13 00:55:04 -0300

    ejercicio1

---
+++ JOAQUIN-PEREZ/UT3/TD/PD1/UT3_PD1/ejercicio1.java
@@ -0,0 +1,28 @@
+public class Ejercicio1 {
+/*
+Son tres nodos, nodo1, nodo 2 y nodo3, nodo2 es el siguiente a nodo1 y nodo3 es el siguiente a nodo2.
+El código crea en una instancia un nuevo nodo, llamemosle nodo4 luego hace nodo4.siguiente lo cual lo inserta al inicio de la lista y apunta como siguiente nodo al nodo1, de momento tenemos 
+nodo4>nodo1>nodo2>nodo3 luego hace nodo2.siguiente nodo3 lo cual entiendo que la deja igual solo saca la referencia al nodo3 y la vuelve a linkear  dando como respuesta la opción A */
+}
+/*Sean nodo1, nodo2 y nodo3 tres nodos consecutivos de una lista (nodo2 es el siguiente a
+nodo1 y nodo3 es el siguiente a nodo2).
+Analice el siguiente fragmento de código (utilice dibujos o diagramas para clarificar qué es lo
+que sucede):
+Nuevo nodo otroNodo
+otroNodo.siguiente  nodo1
+nodo2.siguiente  nodo3
+ */
+/*Ejercicio #2
+Sean nodo1, nodo2 y nodo3 tres nodos consecutivos de una lista (nodo2 es el siguiente a
+nodo1 y nodo3 es el siguiente a nodo2).
+Analice el siguiente fragmento de código (utilice dibujos o diagramas para clarificar qué es lo
+que sucede):
+Nuevo nodo otroNodo
+otroNodo  nodo1.siguiente
+nodo1.siguiente  nodo3 */
+/* inicialmente nodo1. nodo2> nodo3> 
+ * crea  un nuevo nodo 
+ * a ese nodo otroNodo le asigna el siguiente de nodo1
+ * nodo1 > otro nodo 
+ * nodo1>nodo3 borra las demás asignaciones
+*/
